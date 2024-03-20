c6c22347382994d90cc0e2f7037ce74c6b29ab22
Will Metzler <metzlerwill@gmail.com>
2024-03-20 10:35:14 -0400

    code ramen rater functionality

--- src/index.js
+++ src/index.js
@@ -2,23 +2,72 @@
 
 // Callbacks
 const handleClick = (ramen) => {
-  // Add code
-};
+  document.querySelector(".detail-image").src = ramen.image;
+  document.querySelector(".name").textContent = ramen.name;
+  document.querySelector(".restaurant").textContent = ramen.restaurant;
+  document.querySelector("#rating-display").textContent = ramen.rating;
+  document.querySelector("#comment-display").textContent = ramen.comment;
+}
+
+
 
 const addSubmitListener = () => {
-  // Add code
+  document.querySelector("#new-ramen").addEventListener("submit", event => {
+    event.preventDefault();
+    fetch("http://localhost:3000/ramens", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: document.querySelector("form").name.value,
+            restaurant: document.querySelector("form").restaurant.value,
+            image: document.querySelector("form").image.value,
+            rating: document.querySelector("form").rating.value,
+            comment: document.getElementById("new-comment").value,
+        })
+    })
+    .then (response => response.json())
+  .then (ramen => {
+      const img = document.createElement("img");
+      img.src = ramen.image;
+      img.setAttribute("id", ramen.id);
+      document.querySelector("#ramen-menu").append(img);
+      location.reload();
+    });
+})
 }
 
 const displayRamens = () => {
-  // Add code
-};
+  fetch("http://localhost:3000/ramens")
+  .then(response => response.json())
+  .then(menu => {
+    menu.forEach (e => {
+      const image = document.createElement("img");
+      image.src = e.image;
+      image.setAttribute("id", e.id)
+      document.querySelector("#ramen-menu").append(image);
+    })
+    handleClick(menu[0])
+    const image = document.querySelector("#ramen-menu")
+    image.addEventListener("click", event => {
+      menu.forEach(e => {
+        if (e.id == event.target.id)
+          handleClick(e)
+      })
+    })
+  })
+}
 
 const main = () => {
+  displayRamens();
+  addSubmitListener();
   // Invoke displayRamens here
   // Invoke addSubmitListener here
 }
 
-main()
+main();
 
 // Export functions for testing
 export {
